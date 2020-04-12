f1ba15165fd6eacbfe4a437db79dee244521bb73
Kevin Li <kevinli020508@gmail.com>
2020-04-12 10:37:43 -0400

    Initial commit
    And gate works!

---
+++ src/Main.elm
@@ -0,0 +1,217 @@
+port module Main exposing (main)
+
+import Browser
+import Html exposing (Html)
+import Graph exposing (Graph, NodeId)
+import Graph.DOT exposing (Rankdir(..))
+import Dict
+import IntDict
+
+
+port showCircuit : String -> Cmd msg
+
+
+type alias Model =
+  { circuit : Circuit }
+
+
+type alias Circuit =
+  { inputPins : List NodeId
+  , outputPins : List NodeId
+  , pins: Graph Bool ()
+  }
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  let
+    nodes =
+      [ False, False ] ++ List.repeat 7 False
+    
+    edges =
+      [ (0, 2)
+      , (1, 3)
+      , (2, 4)
+      , (3, 4)
+      , (4, 5)
+      , (4, 6)
+      , (5, 7)
+      , (6, 7)
+      , (7, 8)
+      ]
+    
+    inputPins =
+      [ 0, 1 ]
+    
+    outputPins =
+      [ 8 ]
+    
+    pins =
+      Graph.fromNodeLabelsAndEdgePairs nodes edges
+    
+    styles =
+      { rankdir = LR
+      , graph = ""
+      , node = ""
+      , edge = ""
+      }
+
+    nodeLabelToString label =
+      if label then "1" else "0"
+
+    edgeLabelToString =
+      always ""
+    
+    nodeLabelToAttributes label =
+      Dict.fromList <|
+        [ ("label", nodeLabelToString label)
+        ]
+        ++ if label then
+          [ ("color", "green") ]
+        else
+          []
+    
+    edgeLabelToAttributes label =
+      Dict.fromList
+        [ ("label", edgeLabelToString label)
+        ]
+    
+    circuit =
+      eval { inputPins = inputPins 
+      , pins = pins
+      , outputPins = outputPins
+      }
+  in
+  ({ circuit =
+    circuit
+  }
+  , showCircuit <| Graph.DOT.outputWithStylesAndAttributes styles nodeLabelToAttributes edgeLabelToAttributes circuit.pins
+  )
+
+
+type Msg
+  = NoOp
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  (model, Cmd.none)
+
+
+eval : Circuit -> Circuit
+eval c =
+  let
+    startIds =
+      List.map
+      (\inputId ->
+        let
+          nextId =
+            Graph.get inputId c.pins
+              |> Maybe.andThen
+                (\ctx -> Maybe.map Tuple.first <| List.head <| IntDict.toList ctx.outgoing)
+        in
+        Maybe.withDefault 0 nextId
+      )
+      c.inputPins
+  in
+  evalHelper startIds c
+
+
+evalHelper : List NodeId -> Circuit -> Circuit
+evalHelper ids c =
+  if List.length ids == 0 then
+    c
+  else
+    let
+      (newValues, nextIdLists) =
+        List.unzip <| List.map
+          (\id ->
+            case Graph.get id c.pins of
+              Just ctx ->
+                let
+                  inputIds =
+                    List.map Tuple.first <| IntDict.toList ctx.incoming
+                  newValue =
+                    case inputIds of
+                      headId :: tailIds ->
+                        let
+                          firstValue =
+                            getValue headId c
+                        in
+                        case tailIds of
+                          [] ->
+                            firstValue
+                          secondId :: _ ->
+                            nand firstValue (getValue secondId c)
+                      [] ->
+                        ctx.node.label
+                  nextIdList =
+                    List.map Tuple.first <| IntDict.toList ctx.outgoing
+                in
+                (newValue, nextIdList)
+              Nothing ->
+                (False, []) -- impossible
+            )
+            ids
+      updatedCircuit =
+        List.foldl
+          (\(id, newValue) newCircuit ->
+            { newCircuit
+              | pins =
+                Graph.update
+                  id
+                  (\currentCtx ->
+                    Maybe.map
+                      (\ctx ->
+                        let
+                          node = ctx.node
+                        in
+                        { ctx
+                          | node = { node | label = newValue }
+                        }
+                      )
+                      currentCtx
+                  )
+                  newCircuit.pins
+            }
+          )
+          c
+          (List.map2 Tuple.pair ids newValues)
+      nextIds =
+        List.concat nextIdLists
+    in
+    evalHelper nextIds updatedCircuit
+
+
+nand : Bool -> Bool -> Bool
+nand a b =
+  not (a && b)
+
+
+getValue : NodeId -> Circuit -> Bool
+getValue id c =
+  case Graph.get id c.pins of
+    Just ctx ->
+      ctx.node.label
+    Nothing ->
+      False
+
+
+view : Model -> Html Msg
+view _ =
+  Html.div [] []
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+  Sub.none
+
+
+main : Program () Model Msg
+main =
+  Browser.element
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
